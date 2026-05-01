35756173c9f51c5c1207ad273ad09cdc2fe7a313
subol <subolkumar8822@gmail.com>
2026-05-01 15:34:20 +0530

    commit Test java file...

---
+++ src/practicejavaprogram/Test.java
@@ -0,0 +1,14 @@
+package practicejavaprogram;
+
+public class Test
+{
+    public static void main(String [] args)
+    {
+        String str = "I love coding";
+        char  ch[]  =  str.toCharArray();
+        for(char c : ch)
+        {
+            System.out.println(c);
+        }
+    }
+}
